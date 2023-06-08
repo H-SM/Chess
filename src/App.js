3c924b5b27000feadaefe9f2ba7d8ae4de324804
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-06-08 08:16:28 +0530

    Implemented the grid & made the chess-board

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Chessboard from './component/chessboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 
-          <br/> This is our Chess-Game
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="app">
+      <Chessboard/>
     </div>
   );
 }
